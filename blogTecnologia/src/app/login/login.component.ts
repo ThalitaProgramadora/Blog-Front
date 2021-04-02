e7a8f77256210c5de5a3ef98aad7f93612fd5f71
Thalita <thalita.programadora@gmail.com.br>
2021-04-02 17:32:23 -0300

    foi feito o consumo de API-LOGIN E CADASTRO

--- blogTecnologia/src/app/login/login.component.ts
+++ blogTecnologia/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/User.login';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userLogin: UserLogin = new UserLogin()
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+        this.userLogin = resp
+        environment.token = this.userLogin.token
+        environment.nome = this.userLogin.nome
+        environment.foto = this.userLogin.foto
+        environment.id = this.userLogin.id
+        
+        //console.log(environment.token)
+        //console.log(environment.nome)
+        //console.log(environment.foto)
+        //console.log(environment.id)
 
-  ngOnInit(): void {
+        this.userLogin.foto
+        this.router.navigate(['/inicio'])
+    },erro => {
+      if(erro.status == 500){
+        alert('usuário ou senha inválidos!!')
+      }
+    })
   }
 
 }
