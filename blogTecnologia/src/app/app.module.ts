701e15b1583436e70a44cf482820121f8ede061c
Thalita <thalita.programadora@gmail.com.br>
2021-04-07 09:00:21 -0300

    componente tema sem consumo de api

--- blogTecnologia/src/app/app.module.ts
+++ blogTecnologia/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import {HashLocationStrategy, LocationStrategy } from '@angular/common'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,6 +11,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 
@@ -22,7 +24,8 @@ import { InicioComponent } from './inicio/inicio.component';
 
     MenuComponent,
     RodapeComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent
 
   ],
   imports: [
@@ -31,7 +34,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- blogTecnologia/src/app/menu/menu.component.ts
+++ blogTecnologia/src/app/menu/menu.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  nome = environment.nome
+  foto = environment.foto
+  constructor(
+    private router : Router
+  ) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(){
   }
-
+  sair(){
+     this.router.navigate(['/login'])
+     environment.token = ''
+     environment.nome = ''
+     environment.foto = ''
+     environment.id = 0
+  }
 }
