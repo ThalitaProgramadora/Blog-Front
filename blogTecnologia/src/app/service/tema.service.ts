0fdf9b911bbd1935ecc0f7c37bb95dca3d775cab
Thalita <thalita.programadora@gmail.com.br>
2021-04-08 11:48:32 -0300

    arrumando codigo

--- blogTecnologia/src/app/service/tema.service.ts
+++ blogTecnologia/src/app/service/tema.service.ts
@@ -1,10 +1,4 @@
-
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
-
-import { Tema } from '../model/Tema';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +6,10 @@ import { Tema } from '../model/Tema';
 export class TemaService {
 
   constructor(
-    private http: HttpClient
+    
   ) { }
 
-  token = {
-    headers: new HttpHeaders().set('Authorization', environment.token)
-  }
 
-  getAllTema(): Observable<Tema[]> {
-    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
-  }
-
-
-  postTema(tema: Tema): Observable<Tema> {
-    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
-  }
 
   
 }
--- blogTecnologia/src/app/tema/tema.component.ts
+++ blogTecnologia/src/app/tema/tema.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
-import { Tema } from '../model/Tema';
-import { TemaService } from '../service/tema.service';
+
 
 
 @Component({
@@ -11,27 +8,17 @@ import { TemaService } from '../service/tema.service';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
-  tema: Tema = new Tema()
-  listaTemas: Tema[]
+
 
   constructor(
-    private router: Router,
-    private temaService: TemaService
+  
   ) { }
 
   ngOnInit() {
     window.scroll(0, 0)
-    if(environment.token == ''){
-      alert('Sua sessão expirou. Por favor faça o login novamente!')
-      this.router.navigate(['/login'])
-     }
+   
   }
 
-  cadastrar(){
-    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
-      this.tema = resp
-      alert("Tema registrado com sucesso!")
-    })
-  }
+  
 
 }
